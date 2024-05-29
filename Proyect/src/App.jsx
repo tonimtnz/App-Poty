87bf048420d0876c29d17df51282b0f0838188df
tonimtnz <tonimartinezlluesma@gmail.com>
2024-05-29 17:00:24 +0200

    no hay cambios

--- Proyect/src/App.jsx
+++ Proyect/src/App.jsx
@@ -1,17 +1,12 @@
 import { FetchUserProfileData } from "./components/FetchUserProfileData";
 import { Routes, Route } from "react-router-dom";
 // import { UserProfile } from "./pages/UserProfile";
-// import { useEffect } from "react";
 import { Footer } from "./pages/Footer";
 import { Header } from "./pages/Header";
-// import { useGetUserData } from "./components/useGetUserData";
+
 
 function App() {
-  // const { GetUserData } = useGetUserData();
-
-  // useEffect(() => {
-  //   GetUserData("llues_mtnz");
-  // }, []);
+  
   return (
     <>
       <Header />
