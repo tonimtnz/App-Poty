b71ad4b74db6bdc27f298ada0feca696d341c15d
Richard Suarez <richardmsc24@gmail.com>
2024-05-30 22:24:04 +0200

    looking for styles

--- Proyect/src/components/MusicPlaceholder.jsx
+++ Proyect/src/components/MusicPlaceholder.jsx
@@ -1,22 +1,10 @@
+import "../styles/recommendedMusicPlaceholder.css";
+
 export function MusicPlaceholder() {
   return (
-    <div
-      style={{
-        backgroundColor: "#727272",
-        color: "#ffffff",
-        fontFamily: "Poppins",
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "center",
-        alignItems: "center",
-        gap: "1.5rem",
-        padding: "2rem 0",
-        borderRadius: "20px",
-        height: "352px",
-      }}
-    >
+    <div className="placeholder-div">
       <img
-        style={{ borderRadius: "100%", width: "140px" }}
+        className="placeholder-img-recommendedMusic"
         src="./src/assets/MusicPlaceholder.jpg"
       />
       <h3>Elige una canción</h3>
