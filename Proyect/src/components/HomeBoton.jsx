5bd6076df8334404d1261b96cd4d074f6b64e94a
LuisGrandini <grandiniluis18@gmail.com>
2024-05-30 17:46:12 +0200

    botones

---
+++ Proyect/src/components/HomeBoton.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function HomeBoton() {
+  return (
+    <div className='botones-del-home'>
+      <button className='boton-recomendaciones'>Recomendaciones</button>
+      <div className='botones-inferiores'>
+          <button className='boton-playlist'>Mis Playlists</button>
+          <button className='boton-artista'>Artistas</button>
+      </div>
+    </div>
+  )
+}
+
+export default HomeBoton
