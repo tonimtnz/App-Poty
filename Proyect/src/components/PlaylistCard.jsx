7698eac4352e2a0616d602958169b085fb614e0a
tonimtnz <tonimartinezlluesma@gmail.com>
2024-05-31 12:04:13 +0200

    ya no rompe

--- Proyect/src/components/PlaylistCard.jsx
+++ Proyect/src/components/PlaylistCard.jsx
@@ -1,25 +1,35 @@
 import "../styles/UserProfile.css";
+import { RenderImage } from "./RenderImage";
+
 
 export function PlaylistCard({ playlistInfo }) {
   function getImageId(playlistInfo) {
-    const partes = playlistInfo.image_url.split(":");
-    const ids = partes.slice(2).join("");
-    return ids;
-  }
+    console.log(playlistInfo)
+     if(!playlistInfo.image_url){
+      const idPlace = '../assets/playlist-holder.jpg'
+      return idPlace;
+     }else{
+       const initPartes = playlistInfo.image_url;
+       const partes = playlistInfo.image_url.split(":");
+       if(partes[0] === "https" ){
+         const ids = initPartes
+         console.log(ids)
+         return ids;
+       }else{
+         const ids = partes.slice(2).join("");
+         console.log(ids)
+         return ids;
+       }
+     }
+    }
+  
 
   const image_id = getImageId(playlistInfo);
 
   return (
     <>
       <div className="playlist-card">
-        {image_id && <img
-          className="img-playlist"
-          src={
-            image_id.length > 22
-              ? `https://mosaic.scdn.co/300/${image_id}`
-              : `https://i.scdn.co/image/${image_id}`
-          }
-        />}
+        <RenderImage image_id={image_id}/>
         <h4>{playlistInfo.name}</h4>
       </div>
     </>
---
+++ Proyect/src/components/RenderImage.jsx
@@ -0,0 +1,13 @@
+export function RenderImage({image_id}){
+    console.log(image_id)
+
+    return(
+        <>
+        {image_id === '../assets/playlist-holder.jpg' ? <img className="img-playlist" src="https://runmydev.com/wp-content/uploads/2024/05/Frame-2-1.png"/>
+        :   
+        <img className="img-playlist" src={image_id.length > 44
+            ? `https://mosaic.scdn.co/300/${image_id}`
+            : `https://i.scdn.co/image/${image_id}`}/>}
+        </>
+    )
+}
