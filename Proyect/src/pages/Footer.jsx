c7d17d8b2fdea04905f0bf376a57bfd6a012b087
Richard Suarez <richardmsc24@gmail.com>
2024-06-01 23:16:35 +0200

    uploading changes

--- Proyect/src/pages/Footer.jsx
+++ Proyect/src/pages/Footer.jsx
@@ -1,32 +1,46 @@
-import '../styles/footer.css'
+import "../styles/footer.css";
 import { LogoHeader } from "../components/header/LogoHeader";
 
-export function Footer(){
-
-    return(
-        <footer className="footer-mc">
-            <LogoHeader/>
-            <p>La información y el uso de la API de Spotify en esta aplicación están destinados únicamente para propósitos académicos y de aprendizaje.</p>
-            <h4>Desarrollado por:</h4>
-            <ul className="members-list">
-                <li>
-                    <a href="#" target='_blank'>Richard Suárez</a>
-                </li>
-                <li>
-                    <a href="#" target='_blank'>Pedro Grandini</a>
-                </li>
-                <li>
-                    <a href="#" target='_blank'>Luis Grandini</a>
-                </li>
-                <li>
-                    <a href="#" target='_blank'>Antonio Martínez</a>
-                </li>
-                <li>
-                    <a href="#" target='_blank'>Jairo Romero</a>
-                </li>
-            </ul>
-            <hr></hr>
-            <h3>A Develhope Proyect that rules 🥵</h3>
-        </footer>
-    )
+export function Footer() {
+  return (
+    <footer>
+      <div className="footer-mc">
+        <LogoHeader />
+        <p>
+          La información y el uso de la API de Spotify en esta aplicación están
+          destinados únicamente para propósitos académicos y de aprendizaje.
+        </p>
+        <h4>Desarrollado por:</h4>
+        <ul className="members-list">
+          <li>
+            <a href="#" target="_blank">
+              Richard Suárez
+            </a>
+          </li>
+          <li>
+            <a href="#" target="_blank">
+              Pedro Grandini
+            </a>
+          </li>
+          <li>
+            <a href="#" target="_blank">
+              Luis Grandini
+            </a>
+          </li>
+          <li>
+            <a href="#" target="_blank">
+              Antonio Martínez
+            </a>
+          </li>
+          <li>
+            <a href="#" target="_blank">
+              Jairo Romero
+            </a>
+          </li>
+        </ul>
+        <hr></hr>
+        <h3>A Develhope Proyect that rules 🥵</h3>
+      </div>
+    </footer>
+  );
 }
--- Proyect/src/pages/Home.jsx
+++ Proyect/src/pages/Home.jsx
@@ -1,23 +1,19 @@
-import { HeaderUserSearch } from '../components/header/HeaderUserSearch.jsx';
-import { HomeLogo } from '../components/HomeLogo.jsx';
-import  PostForm  from '../components/PostForm.jsx';
-import  PostList  from '../components/PostList.jsx';
+// import { HeaderUserSearch } from "../components/header/HeaderUserSearch.jsx";
+import PostForm from "../components/PostForm.jsx";
+import PostList from "../components/PostList.jsx";
 
-export function Home(){
-
-    return(
-      <div className="home-container">
-        <HeaderUserSearch/>
-        {/* <div className="logo-container">
+export function Home() {
+  return (
+    <div className="home-container">
+      {/* <HeaderUserSearch/> */}
+      {/* <div className="logo-container">
           <HomeLogo/>
         </div> */}
-        <div className="home-post-container">
+      <div className="home-post-container">
         <PostForm />
         <PostList />
-        </div>
-        <div className="home-buttons-container">
-          
-        </div>
       </div>
-    )
+      <div className="home-buttons-container"></div>
+    </div>
+  );
 }
