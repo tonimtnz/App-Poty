73bbe2875e70bae814188f2cd16b226cf54c12a7
tonimtnz <tonimartinezlluesma@gmail.com>
2024-06-02 18:55:22 +0200

    todo ok

--- Proyect/src/pages/Home.jsx
+++ Proyect/src/pages/Home.jsx
@@ -5,15 +5,13 @@ import PostList from "../components/PostList.jsx";
 export function Home() {
   return (
     <div className="home-container">
-      {/* <HeaderUserSearch/> */}
-      {/* <div className="logo-container">
-          <HomeLogo/>
-        </div> */}
       <div className="home-post-container">
         <PostForm />
         <PostList />
       </div>
-      <div className="home-buttons-container"></div>
+      <div className="home-buttons-container">
+        
+      </div>
     </div>
   );
 }
